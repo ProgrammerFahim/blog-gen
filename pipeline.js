bd82b016cc07662625fb94eeeb3a22bfecbceeef
Fahim Faisal <fahimfaisal76@gmail.com>
2025-02-04 18:32:17 +0600

    Remove output logging and refactor tabs to spaces

--- pipeline.js
+++ pipeline.js
@@ -15,18 +15,18 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 function readDirectory(dir, fileList = []) {
-	const listing = fs.readdirSync(dir, { withFileTypes: true });
+    const listing = fs.readdirSync(dir, { withFileTypes: true });
 
-	for (let file of listing) {
-		let fullName = path.join(file.path, file.name);
-		if (file.isFile()) {
-			fileList.push(fullName);
-		} else if (file.isDirectory()) {
-			readDirectory(fullName, fileList);
-		}
-	}
+    for (let file of listing) {
+        let fullName = path.join(file.path, file.name);
+        if (file.isFile()) {
+            fileList.push(fullName);
+        } else if (file.isDirectory()) {
+            readDirectory(fullName, fileList);
+        }
+    }
 
-	return fileList;
+    return fileList;
 }
 
 config = fs.readFileSync("config.json", "utf-8");
@@ -36,69 +36,68 @@ inputFiles = readDirectory(config.src);
 outputFiles = []
 
 for (const file of inputFiles) {
-	let resourcePath = getResourcePath(file, config.src);
-	let inputFileParts = path.parse(file);
-	let inputDirRelative = path.relative(config.src, inputFileParts.dir);
+    let resourcePath = getResourcePath(file, config.src);
+    let inputFileParts = path.parse(file);
+    let inputDirRelative = path.relative(config.src, inputFileParts.dir);
 
-	let outputDir = path.join(config.tgt, resourcePath);
-	let outputFile = path.join(outputDir, "index.html");
+    let outputDir = path.join(config.tgt, resourcePath);
+    let outputFile = path.join(outputDir, "index.html");
 
-	outputFiles.push(outputFile);
-	console.log(`${file} ----> ${outputFile}`);
+    outputFiles.push(outputFile);
 }
 
 
 for (let i = 0; i < inputFiles.length; i++) {
-	try {
-		fs.mkdirSync(path.parse(outputFiles[i]).dir, { recursive: true });
-	} catch(err) {
-		if (err.code != 'EEXIST') {
-			console.error(err);
-			throw new Error("Problem with creating output folder");
-		}
-	}
+    try {
+        fs.mkdirSync(path.parse(outputFiles[i]).dir, { recursive: true });
+    } catch(err) {
+        if (err.code != 'EEXIST') {
+            console.error(err);
+            throw new Error("Problem with creating output folder");
+        }
+    }
 
 
-	let args = "-s --katex --no-highlight --template=template/default.html";
-	(function (outFile) {
-		pandoc(inputFiles[i], args, function(err, res) {
-			if (err) {
-				console.error(err);
-			}
-			
-			dom = new JSDOM(res);
+    let args = "-s --katex --no-highlight --template=template/default.html";
+    (function (outFile) {
+        pandoc(inputFiles[i], args, function(err, res) {
+            if (err) {
+                console.error(err);
+            }
+            
+            dom = new JSDOM(res);
 
-			// format code
-			codeBlocks = dom.window.document.querySelectorAll("pre code");
-			if (codeBlocks.length > 0) {
-				codeBlocks.forEach(hljs.highlightElement);
+            // format code
+            codeBlocks = dom.window.document.querySelectorAll("pre code");
+            if (codeBlocks.length > 0) {
+                codeBlocks.forEach(hljs.highlightElement);
 
-				// Insert link to code stylesheet
-				codeLink = dom.window.document.createElement("link");
-				codeLink.rel = "stylesheet";
-				codeLink.href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/github-dark.min.css";
-				dom.window.document.head.appendChild(codeLink);
-			}
+                // Insert link to code stylesheet
+                codeLink = dom.window.document.createElement("link");
+                codeLink.rel = "stylesheet";
+                codeLink.href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/github-dark.min.css";
+                dom.window.document.head.appendChild(codeLink);
+            }
 
-			// format math
-			document = dom.window.document;
-			mathElements = dom.window.document.getElementsByClassName("math");
-			macros = [];
-			for (var i = 0; i < mathElements.length; i++) {
-				texText = mathElements[i].firstChild;
-				if (mathElements[i].tagName == "SPAN") {
-					katex.render(texText.data, mathElements[i], {
-						displayMode:mathElements[i].classList.contains('display'),
-						throwOnError: false,
-						macros: macros,
-						fleqn: false
-					});
-				}
-			}
+            // format math
+            document = dom.window.document;
+            mathElements = dom.window.document.getElementsByClassName("math");
+            macros = [];
+            for (var i = 0; i < mathElements.length; i++) {
+                texText = mathElements[i].firstChild;
+                if (mathElements[i].tagName == "SPAN") {
+                    katex.render(texText.data, mathElements[i], {
+                        displayMode:mathElements[i].classList.contains('display'),
+                        throwOnError: false,
+                        macros: macros,
+                        fleqn: false
+                    });
+                }
+            }
 
-			// write file to output
-			formattedHtml = dom.serialize();
-			fs.writeFileSync(outFile, formattedHtml);
-		});
-	})(outputFiles[i]);
+            // write file to output
+            formattedHtml = dom.serialize();
+            fs.writeFileSync(outFile, formattedHtml);
+        });
+    })(outputFiles[i]);
 }
--- resource_path.js
+++ resource_path.js
@@ -9,50 +9,50 @@ const fs = require("fs");
 const path = require("path");
 
 function getResourcePath(filePath, src) {
-	file = fs.readFileSync(filePath, "utf-8");
+    file = fs.readFileSync(filePath, "utf-8");
 
-	re = /^(---\n)([\s\S]*?)(\.\.\.\n|---\n)/gm
-	yamlBlocks = file.matchAll(re);
+    re = /^(---\n)([\s\S]*?)(\.\.\.\n|---\n)/gm
+    yamlBlocks = file.matchAll(re);
 
-	yamlData = {} 
-	for (const yamlBlock of yamlBlocks) {
-		yaml = yamlBlock[2];
-		yaml = jsYaml.load(yaml);	
-		yamlData = {...yamlData, ...yaml};
-	}
+    yamlData = {} 
+    for (const yamlBlock of yamlBlocks) {
+        yaml = yamlBlock[2];
+        yaml = jsYaml.load(yaml);   
+        yamlData = {...yamlData, ...yaml};
+    }
 
-	let fileName = "";
-	let permCharsRe = /^[\w- ]*/g
+    let fileName = "";
+    let permCharsRe = /^[\w- ]*/g
 
-	if ("url_path" in yamlData) {
+    if ("url_path" in yamlData) {
 
-		// if `url_path` variable is available in metadata block,
-		// use it directory. Assume it is well-structured URL
-		fileName = yamlData["url_path"];
-	} else if ("title" in yamlData) {
+        // if `url_path` variable is available in metadata block,
+        // use it directory. Assume it is well-structured URL
+        fileName = yamlData["url_path"];
+    } else if ("title" in yamlData) {
 
-		// If not `url_path`, repurpose the `title` to be the URL
-		fileName = yamlData["title"];
-		fileName = fileName.match(permCharsRe)[0]
-					.trim().toLowerCase().replaceAll(/  */g, '-');
-	} else {
+        // If not `url_path`, repurpose the `title` to be the URL
+        fileName = yamlData["title"];
+        fileName = fileName.match(permCharsRe)[0]
+                    .trim().toLowerCase().replaceAll(/  */g, '-');
+    } else {
 
-		// If not `url_path` AND `title`, try using the name
-		// of the file itself
-		filePathParts = path.parse(filePath);
-		fileName = filePathParts.name;
+        // If not `url_path` AND `title`, try using the name
+        // of the file itself
+        filePathParts = path.parse(filePath);
+        fileName = filePathParts.name;
 
-		// If name of the file is `index`, use name of the directory
-		// in which it resides (unless it is directly in the src directory)
-		filePathRelative = path.parse(path.relative(src, filePathParts.dir));
-		if (fileName == 'index' && filePathRelative.name != '')
-			fileName = filePathRelative.name;
+        // If name of the file is `index`, use name of the directory
+        // in which it resides (unless it is directly in the src directory)
+        filePathRelative = path.parse(path.relative(src, filePathParts.dir));
+        if (fileName == 'index' && filePathRelative.name != '')
+            fileName = filePathRelative.name;
 
-		fileName = fileName.match(permCharsRe)[0]
-					.trim().toLowerCase().replaceAll(/  */g, '-');
-	}
+        fileName = fileName.match(permCharsRe)[0]
+                    .trim().toLowerCase().replaceAll(/  */g, '-');
+    }
 
-	return fileName;
+    return fileName;
 }
 
 module.exports = getResourcePath;
